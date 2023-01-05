bda1f3cf87d7ada23ab8961b899d7fcdd29e12e8
DragonGod65 <102479789+DragonGod65@users.noreply.github.com>
2023-01-05 10:21:28 +0100

    Flashy

--- src/main/java/com/tetrastudios/fp_lib/client/FP_libClient.java
+++ src/main/java/com/tetrastudios/fp_lib/client/FP_libClient.java
@@ -10,6 +10,7 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.minecraft.client.particle.FlameParticle;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 
@@ -18,12 +19,25 @@ public class FP_libClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ParticleFactoryRegistry.getInstance().register(CustomParticleLoader.CITRINE_PARTICLE, CitrineFactory.Factory::new);
 
-        ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) -> {
-            registry.register(new Identifier(FP_lib.MOD_ID, "particle/red_flash"));
-        }));
-
+        //Flashes
         ParticleFactoryRegistry.getInstance().register(FlashParticles.RED_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.ORANGE_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.YELLOW_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.BLACK_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.BLUE_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.BROWN_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.CYAN_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.GRAY_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.GREEN_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.LIGHT_BLUE_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.LIGHT_GRAY_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.LIME_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.MAGENTA_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.PINK_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.WHITE_FLASH, FlameParticle.Factory::new);
+        ParticleFactoryRegistry.getInstance().register(FlashParticles.PURPLE_FLASH, FlameParticle.Factory::new);
+        //Custom
+        ParticleFactoryRegistry.getInstance().register(CustomParticleLoader.CITRINE_PARTICLE, CitrineFactory.Factory::new);
     }
 }
--- src/main/java/com/tetrastudios/fp_lib/particle/FlashParticles.java
+++ src/main/java/com/tetrastudios/fp_lib/particle/FlashParticles.java
@@ -8,41 +8,41 @@ import net.minecraft.util.registry.Registry;
 
 public class FlashParticles {
     public static final DefaultParticleType RED_FLASH = FabricParticleTypes.simple();
-    //  public static final DefaultParticleType ORANGE_FLASH = FabricParticleTypes.simple();
-    //  public static final DefaultParticleType YELLOW_FLASH = FabricParticleTypes.simple();
-    //  public static final DefaultParticleType LIME_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType GREEN_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType CYAN_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType LIGHT_BLUE_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType BLUE_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType PURPLE_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType MAGENTA_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType PINK_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType BROWN_FLASH = FabricParticleTypes.simple();
-    //  public static final DefaultParticleType BLACK_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType GRAY_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType LIGHT_GRAY_FLASH = FabricParticleTypes.simple();
-    //   public static final DefaultParticleType WHITE_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType ORANGE_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType YELLOW_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType LIME_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType GREEN_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType CYAN_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType LIGHT_BLUE_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType BLUE_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType PURPLE_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType MAGENTA_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType PINK_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType BROWN_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType BLACK_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType GRAY_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType LIGHT_GRAY_FLASH = FabricParticleTypes.simple();
+    public static final DefaultParticleType WHITE_FLASH = FabricParticleTypes.simple();
 
 
 
     public static void registerParticles() {
         Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "red_flash"), RED_FLASH);
-        //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "orange_flash"), ORANGE_FLASH);
-     //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "yellow_flash"), YELLOW_FLASH);
-        //     Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "lime_flash"), LIME_FLASH);
-        //     Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "green_flash"), GREEN_FLASH);
-        //     Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "cyan_flash"), CYAN_FLASH);
-        //     Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "light_blue_flash"), LIGHT_BLUE_FLASH);
-        //     Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "blue_flash"), BLUE_FLASH);
-        //     Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "purple_flash"), PURPLE_FLASH);
-        //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "magenta_flash"), MAGENTA_FLASH);
-        //   //   //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "pink_flash"), PINK_FLASH);
-        //  Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "brown_flash"), BROWN_FLASH);
-        //  Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "black_flash"), BLACK_FLASH);
-        //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "gray_flash"), GRAY_FLASH);
-        //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "light_gray_flash"), LIGHT_GRAY_FLASH);
-        //   Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "white_flash"), WHITE_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "orange_flash"), ORANGE_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "yellow_flash"), YELLOW_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "lime_flash"), LIME_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "green_flash"), GREEN_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "cyan_flash"), CYAN_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "light_blue_flash"), LIGHT_BLUE_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "blue_flash"), BLUE_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "purple_flash"), PURPLE_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "magenta_flash"), MAGENTA_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "pink_flash"), PINK_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "brown_flash"), BROWN_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "black_flash"), BLACK_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "gray_flash"), GRAY_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "light_gray_flash"), LIGHT_GRAY_FLASH);
+        Registry.register(Registry.PARTICLE_TYPE, new Identifier(FP_lib.MOD_ID, "white_flash"), WHITE_FLASH);
 
 
 
